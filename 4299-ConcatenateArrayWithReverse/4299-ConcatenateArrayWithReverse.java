1ab995e2b824df6fc5ffff9bb9a5b483e75cd4a7
Mercy1105 <celine11mercy@gmail.com>
2026-07-14 14:15:17 +0530

    Create 4299-ConcatenateArrayWithReverse

---
+++ 4299-ConcatenateArrayWithReverse/4299-ConcatenateArrayWithReverse.java
@@ -0,0 +1,15 @@
+// Last updated: 14/07/2026, 14:15:15
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+        }
+        int idx = n;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[idx++] = nums[i];
+        }
+        return ans;
+    }
+}
