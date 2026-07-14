3443bb78fb2adfd6e6d3135da662e0daa195abce
Mercy1105 <celine11mercy@gmail.com>
2026-07-14 14:16:49 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,18 @@
+// Last updated: 14/07/2026, 14:16:48
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true; 
+        
+    }
+}
