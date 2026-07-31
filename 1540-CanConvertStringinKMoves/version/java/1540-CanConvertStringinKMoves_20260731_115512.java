aa16bdd9ea469b0acfdfd09445d89482b11ee98c
Mercy1105 <celine11mercy@gmail.com>
2026-07-31 11:55:14 +0530

    Create 1540-CanConvertStringinKMoves

---
+++ 1540-CanConvertStringinKMoves/version/java/1540-CanConvertStringinKMoves_20260731_115512.java
@@ -0,0 +1,19 @@
+// Last updated: 31/07/2026, 11:55:12
+1class Solution {
+2    public boolean canConvertString(String s, String t, int k) {
+3       if (s.length() != t.length()) {
+4            return false;
+5        }
+6        int[] character = new int[26];
+7        for (int i = 0; i < s.length(); i++) {
+8            int shift = (t.charAt(i) - s.charAt(i) + 26) % 26;
+9            character[shift]++;
+10        }
+11        for (int i = 1; i < 26; i++) {
+12            if (i + (character[i] - 1) * 26 > k) {
+13                return false;
+14            }
+15        }
+16        return true; 
+17    }
+18}
